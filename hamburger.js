bfc8ff9bff2c4c72674d9c7c8da39fe168541967
Rabea Akter Sumi <rabeyaaktersumi911@gmail.com>
2026-02-21 18:36:30 +0600

    added boldo homepage full code

---
+++ hamburger.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("nav-link").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function closeNav() {
+  document.getElementById("nav-link").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
